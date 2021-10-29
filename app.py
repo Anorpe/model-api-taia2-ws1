2cfbcfb188248b3cdb133a83105872022b82f79f
anorpe <aorregop@unal.edu.co>
2021-10-29 16:26:55 -0500

    a

--- app.py
+++ app.py
@@ -23,5 +23,9 @@ class Model(Resource):
 
 api.add_resource(Model, '/model')
 
+@app.route('/')
+def index():
+  return "<h1>Bienvenido a Modelos para Tópicos Avanzados de Inteligencia Artificial 2  2021-2</h1>"
+
 if __name__ == '__main__':
      app.run(port='8080',debug = False)
