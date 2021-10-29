6e3724b05d270197a47bd6bc980a08d5e4a97bf3
anorpe <aorregop@unal.edu.co>
2021-10-29 17:15:07 -0500

    a

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
 from datetime import datetime
+from flask_restful import Resource, Api
+
 app = Flask(__name__)
+api = Api(app)
+
 
 @app.route('/')
 def homepage():
@@ -12,6 +16,31 @@ def homepage():
 
     <img src="http://loremflickr.com/600/400" />
     """.format(time=the_time)
+
+
+
+
+
+class Model(Resource):
+    def get(self):
+        
+        return "Leylisita Jaramillo"
+
+    def post(self):
+
+
+        atributo = request.json['atributo']
+        
+
+        return  {"atributo" : atributo}
+
+
+
+@app.route('/')
+def index():
+  return "<h1>Bienvenido a Modelos para Tópicos Avanzados de Inteligencia Artificial 2  2021-2</h1>"
+
+api.add_resource(Model, '/model')
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
