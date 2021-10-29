8d84046b202a4a1c8595bf478a9129e2492104f1
Andrés Orrego <aorregop@unal.edu.co>
2021-10-29 00:07:16 -0500

    Estructura de API

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+class Model(Resource):
+    def get(self):
+        
+        return "Leylisita Jaramillo"
+
+    def post(self):
+
+
+        atributo = request.json['atributo']
+        
+
+        return  {"atributo" : atributo}
+
+
+
+
+
+api.add_resource(Model, '/model')
+
+if __name__ == '__main__':
+     app.run(port='8080')
